0913263c3a94ac685b86618d74b608a910df6721
okd <nossie531@gmail.com>
2025-07-15 20:52:35 +0900

    Fix #2

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -13,6 +13,32 @@ use translate::translate_ensure_impl;
 mod translate;
 mod util;
 
+/// Trait implementation assertion attribute.
+///
+/// Adding this attribute to an implementation confirms
+/// that the implementation already exists, otherwise
+/// compile error will occur.
+///
+/// # Examples
+///
+/// ```
+/// # use ensure_impl_macro::ensure_impl;
+/// #
+/// trait SomeTrait {}
+/// trait Bound1 {}
+/// trait Bound2 {}
+/// trait Bound3 {}
+/// impl<T> SomeTrait for T
+/// where T: Bound1 + Bound2 + Bound3 {}
+///
+/// struct MyType();
+///
+/// #[ensure_impl]
+/// impl SomeTrait for MyType {}
+/// impl Bound1 for MyType {}
+/// impl Bound2 for MyType {}
+/// impl Bound3 for MyType {}
+/// ```
 #[allow(missing_docs)]
 #[proc_macro_attribute]
 pub fn ensure_impl(attr: pm::TokenStream, item: pm::TokenStream) -> pm::TokenStream {
--- src/lib.rs
+++ src/lib.rs
@@ -8,32 +8,6 @@
 
 pub mod prelude;
 
-/// Trait implementation assertion attribute.
-///
-/// Adding this attribute to an implementation confirms
-/// that the implementation already exists, otherwise
-/// compile error will occur.
-///
-/// # Examples
-///
-/// ```
-/// # use ensure_impl::prelude::*;
-/// #
-/// trait SomeTrait {}
-/// trait Bound1 {}
-/// trait Bound2 {}
-/// trait Bound3 {}
-/// impl<T> SomeTrait for T
-/// where T: Bound1 + Bound2 + Bound3 {}
-///
-/// struct MyType();
-///
-/// #[ensure_impl]
-/// impl SomeTrait for MyType {}
-/// impl Bound1 for MyType {}
-/// impl Bound2 for MyType {}
-/// impl Bound3 for MyType {}
-/// ```
 pub use ensure_impl_macro::ensure_impl;
 
 #[doc(hidden)]
